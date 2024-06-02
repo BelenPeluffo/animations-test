7ef1cd191168daf180476a207e884e421877eecb
Belén Peluffo <belenpeluffolupiano@gmail.com>
2024-06-02 11:36:14 -0300

    refactor: arregla el tema de la route nesting.

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,26 +1,20 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from "../assets/app.module.css";
 import HSL from "../pages/HSL";
 import RGB from "../pages/RGB";
 import Home from "../pages/Home";
 
 export const AppRouter = () => {
-  const DEFAULT_ROUTE = "/home";
   return (
     <BrowserRouter>
       {/* <div style={styles.fill}> */}
       {/* <div style={styles.content}> */}
       <Routes>
-        <Route path="/" element={<Navigate to={DEFAULT_ROUTE} />} />
-        <Route path="/home" element={<Home />}>
+        <Route path="/" element={<Home />}>
           <Route path="hsl/:h/:s/:l" element={<HSL />} />
           <Route path="rgb/:r/:g/:b" element={<RGB />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
-        {/* <Route path="/home" element={<Home />} />
-        <Route path="/hsl/:h/:s/:l" element={<HSL />} />
-        <Route path="/rgb/:r/:g/:b" element={<RGB />} />
-        <Route path="*" element={<ErrorPage />} /> */}
       </Routes>
       {/* </div> */}
       {/* </div> */}
